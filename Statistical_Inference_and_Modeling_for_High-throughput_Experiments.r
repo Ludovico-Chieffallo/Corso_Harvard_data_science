681f7d14f5237ca76c4af20f4203174fc4467926
Ludovico Chieffallo <79975866+Ludovico-Chieffallo@users.noreply.github.com>
2022-07-11 14:59:47 +0200

    Update Statistical_Inference_and_Modeling_for_High-throughput_Experiments.r

--- Statistical_Inference_and_Modeling_for_High-throughput_Experiments.r
+++ Statistical_Inference_and_Modeling_for_High-throughput_Experiments.r
@@ -1,1 +1,19 @@
 
+library(devtools)
+install_github("genomicsclass/GSE5859Subset")
+
+library(GSE5859Subset)
+data(GSE5859Subset)
+
+head(geneExpression)
+
+dim(geneExpression)
+
+
+head(sampleInfo)
+dim(sampleInfo)
+
+identical(colnames(geneExpression), sampleInfo$filename)
+#abbiamo comparato i nomi del file geneespression con il nome delle colonne 
+#del file filenema (il risultato è stato TRUE)
+
